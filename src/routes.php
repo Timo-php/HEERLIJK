ebfac4234cd245c510a5c3577d4165742c4e908a
Timo <timo@heerlijk.nl>
2021-03-23 15:54:18 +0100

    updated package

--- src/routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-Route::get('partnermodule', function(){
-    echo 'Hello from the PartnerModule package!';
-});
-Route::get('partnerselect', 'HEERLIJK\PartnerModule\PartnerSelectController@partnerselect');
-Route::get('otherfunctions', 'HEERLIJK\PartnerModule\PartnerSelectController@otherFunctions');
